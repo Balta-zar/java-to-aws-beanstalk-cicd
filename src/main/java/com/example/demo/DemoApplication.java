7b71a1a7af28b3d6c0d344e52b96f1be435820d0
Bojan Baltic <bojan.baltic@gmail.com>
2023-12-01 18:26:56 +0100

    Add stage properties

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,11 +8,19 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class DemoApplication {
+    @Value("${test_value}")
+    private String ime;
+
     public static void main(String[] args) {
       SpringApplication.run(DemoApplication.class, args);
     }
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
       return String.format("Hello %s!", name);
+    }
+
+    @GetMapping("/")
+    public String root_path(@RequestParam() String name) {
+        return ime;
     }
 }
